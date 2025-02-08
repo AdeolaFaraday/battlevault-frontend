88e17a665c0de0b6c631149c35113050573c442e
Adeola <adexconly@gmail.com>
2025-02-08 20:55:38 +0100

    set up home page design

--- src/components/home/index.tsx
+++ src/components/home/index.tsx
@@ -1,9 +1,22 @@
+import HomeCtaCard from './home-cta-card';
+import PaymentLottie from '../common/lottie/payment.json';
+import PLayLottie from '../common/lottie/play.json';
+
 import './styles.css';
 
 const HomeComponent = () => {
     return <div className='container'>
         <div className='left-container'>Left Side Bar</div>
-        <div className='main-container'>Main Content</div>
+        <div className='main-container'>
+            <div className='home-cta__container'>
+                <HomeCtaCard lottieJson={PaymentLottie} title='Deposit' />
+                <HomeCtaCard lottieJson={PLayLottie} title='Play' />
+            </div>
+            <div className='home-text'>
+                <h3>Hi, <span>Adeola</span></h3>
+                <h2>Play Our Featured Games</h2>
+            </div>
+        </div>
         <div className='right-container'>Right Side Bar</div>
     </div>
 }
