5ec0493e025b19b9713a33a3601fbe5421cfe24e
Adeola <adexconly@gmail.com>
2025-02-09 20:35:40 +0100

    add slider for featured games

--- src/components/home/featured-games-card/card.tsx
+++ src/components/home/featured-games-card/card.tsx
@@ -2,7 +2,9 @@ import './styles.css';
 
 const FeaturedGamesCard = () => {
     return (
-        <div className="feature-games"></div>
+        <div className="feature-games__card keen-slider__slide">
+            <h2>Ludo</h2>
+        </div>
     )
 }
 
