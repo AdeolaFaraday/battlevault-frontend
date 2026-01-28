f8782e95a05738c70922d9bdf9ac1704898f2646
Adeola <adexconly@gmail.com>
2026-01-28 20:17:17 +0100

    handle profile routing from nav

--- src/hooks/auth/useLogout.ts
+++ src/hooks/auth/useLogout.ts
@@ -5,17 +5,20 @@ import { resetState } from '../../lib/redux/authSlice';
 import { useRouter } from 'next/navigation';
 
 export const useLogout = () => {
-    const [logoutMutation, { loading, error }] = useMutation(LOGOUT_MUTATION);
+    const [logoutMutation, { loading, error }] = useMutation(LOGOUT_MUTATION, {
+        onCompleted: () => {
+            // Clear Redux state
+            dispatch(resetState());
+            // Redirect to signin
+            router.push('/signin');
+        }
+    });
     const dispatch = useAppDispatch();
     const router = useRouter();
 
     const logout = async () => {
         try {
             await logoutMutation();
-            // Clear Redux state
-            dispatch(resetState());
-            // Redirect to signin
-            router.push('/signin');
         } catch (err) {
             console.error('Logout error:', err);
         }
