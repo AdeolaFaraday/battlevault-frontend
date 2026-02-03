7a677495269c77b0e115269bcaee7dfbb39c3b27
Adeola <adexconly@gmail.com>
2026-02-03 06:52:14 +0100

    add redirect of create account

--- src/hooks/auth/useSignup.ts
+++ src/hooks/auth/useSignup.ts
@@ -1,13 +1,16 @@
 import { useAlert } from '../common/useAlert';
 import useUserSignup from "@/src/api/auth/useUserSignup";
+import { useRouter } from 'next/navigation';
 
 
 const useSignup = () => {
     const { success, error } = useAlert();
+    const router = useRouter();
     const { createUser, loading } = useUserSignup(
         (data) => {
             if (data?.createUser?.success) {
                 success("Registration Successful", data?.createUser?.message)
+                router.push("/signin")
             } else {
                 error("Registration Failed", data?.createUser?.message)
             }
