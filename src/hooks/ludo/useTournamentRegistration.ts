3aff7ca2009d526a2f9053949b4c6ce0ef543793
Adeola <adexconly@gmail.com>
2026-03-08 07:26:47 +0100

    fix routing previously authenticated users to sign after section expires

--- src/hooks/ludo/useTournamentRegistration.ts
+++ src/hooks/ludo/useTournamentRegistration.ts
@@ -1,11 +1,15 @@
 import { useQuery } from "@apollo/client";
 import { IS_USER_REGISTERED } from "@/src/graphql/game/queries";
 import { IsUserRegisteredResponse } from "@/src/types/tournament";
+import { useAppSelector } from "@/src/lib/redux/hooks";
+import { RootState } from "@/src/lib/redux/store";
 
 export const useTournamentRegistration = (tournamentId: string) => {
+    const { loggedInUserDetails: currentUser, isUserLoggedIn } = useAppSelector((state: RootState) => state.auth);
+    const isAuthenticated = isUserLoggedIn === true;
     const { data, loading, error, refetch } = useQuery<IsUserRegisteredResponse>(IS_USER_REGISTERED, {
         variables: { tournamentId },
-        skip: !tournamentId,
+        skip: !tournamentId || !isAuthenticated,
     });
 
     const isRegistered: boolean = data?.isUserRegistered?.success
